4367894c5691aca6df46e720c82225eda44cfc3c
rexjericho <rexjerichog@gmail.com>
2026-07-10 15:45:18 +0800

    commit all changes

--- pisolocker/lib/widgets/locker_card.dart
+++ pisolocker/lib/widgets/locker_card.dart
@@ -201,7 +201,7 @@ class LockerCard extends StatelessWidget {
     final hours = duration.inHours;
     final minutes = duration.inMinutes.remainder(60);
     if (hours > 0) {
-      return '$hours hr ${minutes} min';
+      return '$hours hr $minutes min';
     }
     return '$minutes min';
   }
